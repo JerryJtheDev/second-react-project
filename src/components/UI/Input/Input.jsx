0f1451b8a9061c01e5f4ceb844c337431c62a050
JerryJunior <jerryjnr385@gmail.com>
2025-08-07 20:53:57 +0100

    Learnt about forwardRefs, growing💯

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,39 @@
+import React, {useRef, useImperativeHandle} from 'react'
+import classes from './Input.module.css';
+import Button from '../Button/Button';
+
+const Input = React.forwardRef((props, ref) => {
+    const inputRef = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate
+        }
+        
+    });
+
+
+    const activate = () => {
+        inputRef.current.focus();
+    }
+
+  return (
+    <div
+              className={`${classes.control} ${
+                props.isValid === false ? classes.invalid : ''
+              }`}
+            >
+              <label htmlFor={props.id}>{props.label}</label>
+              <input
+                ref={inputRef}
+                type={props.type}
+                id={props.id}
+                value={props.value}
+                onChange={props.onChange}
+                onBlur={props.onBlur}
+              />
+    </div> 
+  )
+});
+
+export default Input
